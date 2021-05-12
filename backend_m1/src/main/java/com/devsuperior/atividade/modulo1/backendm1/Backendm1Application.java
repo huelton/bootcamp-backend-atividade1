bc5f201ee262a4df48c079fb33271ce3d4298716
Huelton Santos <hueltondihon@gmail.com>
2021-05-12 15:38:43 -0300

    first commit

---
+++ backend_m1/src/main/java/com/devsuperior/atividade/modulo1/backendm1/Backendm1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.atividade.modulo1.backendm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backendm1Application.class, args);
+	}
+
+}
---
+++ backend_m1/src/test/java/com/devsuperior/atividade/modulo1/backendm1/Backendm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.atividade.modulo1.backendm1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backendm1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
